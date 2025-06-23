81789d3f99995ed913680f7e9cd3c021c277d495
Kenechukwu Akubue <kengoon19@gmail.com>
2025-06-23 20:53:27 +0100

    Remove HomeScreen and associated files
    Deleted `presentation.kv` and `presentation.py` related to the HomeScreen feature. This change likely reflects a decision to refactor, remove unused code, or replace this functionality elsewhere.

--- features/home/presentation.py
+++
@@ -1,9 +0,0 @@
-from os.path import join, dirname, basename
-from kivy.lang import Builder
-from features.basescreen import BaseScreen
-
-Builder.load_file(join(dirname(__file__), basename(__file__).split(".")[0] + ".kv"))
-
-
-class HomeScreen(BaseScreen):
-    pass
